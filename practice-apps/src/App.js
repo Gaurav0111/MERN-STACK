56ccac973ac18fd8ed2a78e8a0fdacc6f946b8e4
Gaurav0111 <garwalgaurav1@gmail.com>
2024-11-03 20:54:57 +0530

    started increament decreament

--- practice-apps/src/App.js
+++ practice-apps/src/App.js
@@ -1,11 +1,13 @@
 // import DigitalClock from "./components/DigitalClock";
-import ToDoList from "./components/To_Do_List/ToDoList";
+// import ToDoList from "./components/To_Do_List/ToDoList";
+import IncrementAndDrecrement from "./components/IncrementAndDrecrement/IncrementAndDrecrement";
 
 
 function App() {
   return (
     // <DigitalClock />
-    <ToDoList />
+    // <ToDoList />
+    <IncrementAndDrecrement />
   );
 }
 
--- practice-apps/src/components/IncrementAndDrecrement/IncrementAndDrecrement.js
+++ practice-apps/src/components/IncrementAndDrecrement/IncrementAndDrecrement.js
@@ -1,1 +1,16 @@
 import React, { useState, useEffect } from 'react';
+import '../../assets/css/IncreamentAndDecrement.css'
+
+const IncrementAndDrecrement = () => {
+
+    
+
+    return(
+        <div className='outerContainer' >
+            <h1>{}</h1>
+        </div>
+    );
+
+}
+
+export default IncrementAndDrecrement;
