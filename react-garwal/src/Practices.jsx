1feedb9814697683a2ce9d059f1c1d53ccefdd49
Gaurav0111 <garwalgaurav1@gmail.com>
2025-02-12 22:04:26 +0530

    day 2 of learning

---
+++ react-garwal/src/Practices.jsx
@@ -0,0 +1,9 @@
+export const Practices = () => {
+    const student = [];
+    return (
+        <>
+            <p>{student.length && "No Student Found"} </p>
+            <p>Number of Student: {student.length} </p>
+        </>
+    )
+}
