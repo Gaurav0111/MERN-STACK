a8aa0be48562b6fa2fb11cd291786a0474d4b75f
Gaurav0111 <garwalgaurav1@gmail.com>
2025-02-15 21:16:10 +0530

    minor

--- react-garwal/src/main.jsx
+++ react-garwal/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+// import 'https://drive.google.com/file/d/1d23DHFuRa0NjmTlGV4QqSsZH8U1kkBv3/view?usp=sharing'
 import App from './App.jsx'
 import { Practices } from './Practices.jsx'
 createRoot(document.getElementById('root')).render(
