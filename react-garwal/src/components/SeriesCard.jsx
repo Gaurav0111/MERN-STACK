94516028afac022befbbe4b63578503bc50c4f39
Gaurav0111 <garwalgaurav1@gmail.com>
2025-02-16 22:16:25 +0530

    completed day 4

--- react-garwal/src/components/SeriesCard.jsx
+++ react-garwal/src/components/SeriesCard.jsx
@@ -1,20 +1,22 @@
 const SeriesCard = (props) => {
     let age = 154;
     const { id, img_url, name, rating, description, cast, genre, watch_url } = props.data;
-    return (<li >
+    return (<li className="card" >
         <div>
             <div>
                 <img src={img_url} alt={img_url} width="40%" height="40%" />
             </div>
-            <h2>Name: {name}</h2>
-            <h2>Rating: {rating}</h2>
-            <p>Summary: {description}</p>
-            {/* <p>Genre: {returnGenre()}</p> */}
-            <p>Genre: {genre}</p>
-            <p>Cast: {cast}</p>
-            <a href={watch_url} target="_blank" >
-                <button>{age > 18 ? "Watch Now" : "Not Available"}</button>
-            </a>
+            <div className="card-content" >
+                <h2>Name: {name}</h2>
+                <h2>Rating: {rating}</h2>
+                <p>Summary: {description}</p>
+                {/* <p>Genre: {returnGenre()}</p> */}
+                <p>Genre: {genre}</p>
+                <p>Cast: {cast}</p>
+                <a href={watch_url} target="_blank" >
+                    <button>{age > 18 ? "Watch Now" : "Not Available"}</button>
+                </a>
+            </div>
         </div>
     </li>)
 }
--- react-garwal/src/main.jsx
+++ react-garwal/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import 'https://drive.google.com/file/d/1d23DHFuRa0NjmTlGV4QqSsZH8U1kkBv3/view?usp=sharing'
+import './index.css'
 import App from './App.jsx'
 import { Practices } from './Practices.jsx'
 createRoot(document.getElementById('root')).render(
