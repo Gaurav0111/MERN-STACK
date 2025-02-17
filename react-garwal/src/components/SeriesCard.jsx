508121b17d5b8a3284048352796cb03482cb4990
Gaurav0111 <garwalgaurav1@gmail.com>
2025-02-17 21:12:26 +0530

    completed day 5

--- react-garwal/src/components/SeriesCard.jsx
+++ react-garwal/src/components/SeriesCard.jsx
@@ -1,24 +1,40 @@
+import "./Netflix.css";
+
 const SeriesCard = (props) => {
     let age = 154;
     const { id, img_url, name, rating, description, cast, genre, watch_url } = props.data;
+    const styling = {
+        margin: "1rem 1rem"
+    }
+    const buttonStyling = {
+        padding: "1.2rem 2.4rem",
+        fontSize: "1.6rem",
+        backgroundColor: `${rating >= 8.5 ? "rgb(49, 175, 175)" : "#ffff00"}`,
+        // backgroundColor: "rgb(49, 175, 175)",
+        borderRadius: "1rem"
+    }
     return (<li className="card" >
         <div>
             <div>
                 <img src={img_url} alt={img_url} width="40%" height="40%" />
             </div>
             <div className="card-content" >
-                <h2>Name: {name}</h2>
-                <h2>Rating: {rating}</h2>
-                <p>Summary: {description}</p>
+                <h3>Name: {name}</h3>
+                <h3>
+                    Rating: <span className={`rating ${rating >= 8.5 ? "super_hit" : "average"}`} >
+                        {rating}
+                    </span>
+                </h3>
+                <p style={styling} >Summary: {description}</p>
                 {/* <p>Genre: {returnGenre()}</p> */}
-                <p>Genre: {genre}</p>
-                <p>Cast: {cast}</p>
+                <p style={styling} >Genre: {genre}</p>
+                <p style={{ margin: "1rem 1rem" }} >Cast: {cast}</p>
                 <a href={watch_url} target="_blank" >
-                    <button>{age > 18 ? "Watch Now" : "Not Available"}</button>
+                    <button style={buttonStyling}>{age > 18 ? "Watch Now" : "Not Available"}</button>
                 </a>
             </div>
         </div>
-    </li>)
+    </li >)
 }
 
 export default SeriesCard;
